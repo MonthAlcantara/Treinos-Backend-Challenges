3f6ef312d2ca073a1258e94581b13e297fdedc36
monthalcantara <montival_junior@yahoo.com.br>
2022-01-27 11:19:30 -0300

    Adicionado projeto desafio ifood

---
+++ IFood/ifood-backend-basic-test/src/main/java/io/github/monthalcantara/ifoodbackendbasictest/IfoodBackendBasicTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.ifoodbackendbasictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodBackendBasicTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodBackendBasicTestApplication.class, args);
+	}
+
+}
---
+++ IFood/ifood-backend-basic-test/src/test/java/io/github/monthalcantara/ifoodbackendbasictest/IfoodBackendBasicTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.ifoodbackendbasictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodBackendBasicTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
